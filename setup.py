14f4a026c77516e0937e11162df922a526d7259a
Brewster Malevich <sbmalev@gmail.com>
2018-03-01 15:56:48 -0700

    Release v0.0.1a0

--- setup.py
+++ setup.py
@@ -10,20 +10,15 @@ setup(
     author='S. Brewster Malevich',
     author_email='malevich@email.arizona.edu',
     url='https://github.com/brews/proxysiphon',
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 1 - Planning',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
 
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
     ],
     keywords='marine paleoclimate',
