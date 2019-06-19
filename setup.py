50c3b07dd35406d70fb9d0f4cbddbb50cd983983
Brewster Malevich <sbmalev@gmail.com>
2019-06-19 08:57:17 -0700

    Release v0.0.1a3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='proxysiphon',
-    version='0.0.1a2',
+    version='0.0.1a3',
     description='Internal lab tool to parse and clean marine sediment proxy data.',
     license='GPLv3',
 
